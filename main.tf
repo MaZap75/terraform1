ae99cf2dc5b569c929e6c405f45f495d905da884
MaZap75 <mzappulla75@gmail.com>
2025-04-23 21:42:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "kubernetes_pod" "example_pod" {
   spec {
     container {
       name  = "container-by-terraform"
-      image = "sha256:001ec5d1b463d8d62c87b11ef9ca64603e6dd2eb88edadcb4f70f1c593aa83af"  # Immagine pubblica da Docker Hub digest
+      image = "httpd-24-centos7@sha256:001ec5d1b463d8d62c87b11ef9ca64603e6dd2eb88edadcb4f70f1c593aa83af"  # Immagine pubblica da Docker Hub digest
       port {
         container_port = 80
       }
