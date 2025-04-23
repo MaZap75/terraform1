fb37b5299f5d6b58eb649427f5059c1e0cbb7eeb
MaZap75 <mzappulla75@gmail.com>
2025-04-23 20:16:46 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "kubernetes_pod" "example_pod" {
+  metadata {
+    name      = "my-app-pod"
+    namespace = "test"
+  }
+
+  spec {
+    container {
+      name  = "container-by-terraform"
+      image = "httpd:latest"  # Immagine pubblica da Docker Hub
+      port {
+        container_port = 80
+      }
+    }
+  }
+}
